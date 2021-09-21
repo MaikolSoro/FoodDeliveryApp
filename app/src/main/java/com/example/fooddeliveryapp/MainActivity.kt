01f4a25eb60fe674daab2c6d34eb0addc31eb778
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-09-20 19:19:30 -0600

    Design the categories

--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,69 +50,99 @@ fun HomeScreen() {
         Column {
             Header()
             Spacer(modifier = Modifier.height(32.dp))
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(156.dp)
-                    .clip(RoundedCornerShape(20.dp))
-                    .background(
-                        Yellow200
+            OrderNowBox()
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(
+                text = "Categories",
+                style = Typography.body1,
+                fontSize = 22.sp,
+                color = BlackTextColor
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            CategoryList(
+                categories = listOf(
+                    CategoryData(
+                        redId = R.drawable.pizza,
+                        title = "Pizza"
+                    ),
+                    CategoryData(
+                        redId = R.drawable.hamburger,
+                        title = "burger"
+                    ),
+                    CategoryData(
+                        redId = R.drawable.drinks,
+                        title = "Drinks"
                     )
-                    .padding(24.dp)
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Column(verticalArrangement = Arrangement.SpaceBetween) {
-                        Text(buildAnnotatedString {
-                            withStyle(
-                                style = SpanStyle(
-                                    color = BlackTextColor,
-                                    fontStyle = Typography.body1.fontStyle
-                                )
-                            ) {
-                                append(
-                                    "The Fastest In\n" +
-                                            "Delivery "
-                                )
-                            }
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Yellow500,
-                                    fontStyle = Typography.body1.fontStyle
-                                )
-                            ) {
-                                append(
-                                    "Food"
-                                )
-                            }
-                        })
-                        Box(
-                            modifier = Modifier
-                                .size(width = 126.dp, height = 40.dp)
-                                .clip(RoundedCornerShape(10.dp))
-                                .background(Yellow500),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(
-                                text = "Order Now",
-                                style = Typography.body1,
-                                fontWeight = FontWeight.SemiBold,
-                                color = Color.White,
-                                fontSize = 14.sp
-                            )
-                        }
+                )
+            )
+        }
+
+    }
+}
+
+@Composable
+fun OrderNowBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(156.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .background(
+                Yellow200
+            )
+            .padding(24.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(verticalArrangement = Arrangement.SpaceBetween) {
+                Text(buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = BlackTextColor,
+                            fontStyle = Typography.body1.fontStyle
+                        )
+                    ) {
+                        append(
+                            "The Fastest In\n" +
+                                    "Delivery "
+                        )
                     }
-                    Image(
-                        painter = painterResource(id = R.drawable.man),
-                        contentDescription = "Man",
-                        modifier = Modifier.size(156.dp)
+                    withStyle(
+                        style = SpanStyle(
+                            color = Yellow500,
+                            fontStyle = Typography.body1.fontStyle
+                        )
+                    ) {
+                        append(
+                            "Food"
+                        )
+                    }
+                })
+                Box(
+                    modifier = Modifier
+                        .size(width = 126.dp, height = 40.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Yellow500),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Order Now",
+                        style = Typography.body1,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White,
+                        fontSize = 14.sp
                     )
                 }
             }
+            Image(
+                painter = painterResource(id = R.drawable.man),
+                contentDescription = "Man",
+                modifier = Modifier.size(156.dp)
+            )
         }
-
     }
 }
 
@@ -164,8 +199,63 @@ fun BoxWithRes(
             tint = iconColor!!
         )
     }
+}
 
+@Composable
+fun CategoryList(categories: List<CategoryData>) {
 
+    val selectedIndex = remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 13.dp), horizontalArrangement = Arrangement.SpaceBetween
+    )
+    {
+        items(categories.size)
+        { index ->
+            CategoryItem(
+                categoryData = categories[index],
+                selectedIndex = selectedIndex,
+                index = index
+            )
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(categoryData: CategoryData, selectedIndex: MutableState<Int>, index: Int) {
+    Box(
+        modifier = Modifier
+            .size(width = 106.dp, height = 146.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .clickable {
+                selectedIndex.value = index
+            }
+            .background(
+                if (selectedIndex.value == index) Yellow500 else CardItemBg
+            ), contentAlignment = Alignment.Center
+    )  {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Icon(
+                painter = painterResource(id = categoryData.redId),
+                contentDescription = categoryData.title,
+                modifier = Modifier.size(48.dp),
+                tint = if (selectedIndex.value == index) Color.White else BlackTextColor
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = categoryData.title,
+                style = Typography.body2,
+                fontSize = 18.sp,
+                color = if (selectedIndex.value == index) Color.White else BlackTextColor
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
